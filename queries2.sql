ae931b9b2fa54cec3efe63918ae33b327ea68f6a
Anton <58876946+KupriyanovAnton@users.noreply.github.com>
2022-08-30 17:51:02 +0400

    Add ways from VKO to CNN

--- queries2.sql
+++ queries2.sql
@@ -22,4 +22,48 @@ FROM
 	join ticket_flights tf using(ticket_no)
 	join flights fl using(flight_id)
 WHERE
-	book_ref = '3B54BB'
+	book_ref = '3B54BB';
+	
+	
+-- Перелеты из VKO в CNN
+
+with recursive ways(dep, arr, dur) as (
+    
+	select 
+        departure_airport, 
+        arrival_airport, 
+        duration, 
+        array[departure_airport::text, arrival_airport::text] path
+    from
+		routes
+    where 
+		departure_airport = 'VKO'
+	
+    union
+	
+    select 
+        r.departure_airport, 
+        r.arrival_airport, 
+        w.dur + r.duration,
+        w.path || r.arrival_airport::text
+    from routes r
+		inner join ways w on w.arr = r.departure_airport
+    where
+        not r.arrival_airport = any(w.path)
+		and w.dur + r.duration <= interval '10 hours'
+        and w.dep <> 'CNN'
+)
+
+select
+	dur,
+    array_to_string(path, '->') path,
+	format('%s years %s monts %s days %s hours %s mins %s secs',  
+		  extract(year from dur), 
+		  extract(month from dur),
+		  extract(day from dur),
+		  extract(hour from dur),
+		  extract(minute from dur),
+		  extract(second from dur)) duration
+from ways
+where arr = 'CNN'
+ORDER BY dur; 
