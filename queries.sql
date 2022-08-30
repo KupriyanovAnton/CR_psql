4fe751c055c16aa3d3006d7577e4d1e7fffa8ddf
Anton <58876946+KupriyanovAnton@users.noreply.github.com>
2022-08-30 18:17:02 +0400

    Fix

--- queries.sql
+++ queries.sql
@@ -1,10 +1,13 @@
+-- 1. Вывести все коды самолетов Аэробус, номер модели которых начинается на А32
 SELECT
 	aircraft_code
 FROM
 	aircrafts_data
 WHERE
-	model->>lang() LIKE 'Аэробус_A32%';
+	model->>'ru' LIKE 'Аэробус_A32%';
 
+
+-- 2. Вывести все аэропорты Москвы с заданием координат Москвы.
 SELECT
 	airport_code,
 	airport_name->>lang() as airport_name,
@@ -13,33 +16,45 @@ SELECT
 FROM
 	airports_data
 WHERE
-	circle '<(37.617222, 55.755833), 1>' @> coordinates;
+	coordinates <@ circle '<(37.61, 55.75), 1>';
 	
+	
+-- 3. Вывести все маршруты, которые длятся 8 и больше часов. 
+--    Вывод должен быть как в примере: PG0168, DME -> UUS, "8 hours, 50 mins"
 SELECT
 	flight_no,
 	concat(departure_airport, '->', arrival_airport) as route,
 	format('%s hours, %s mins', 
-		(EXTRACT(epoch from scheduled_arrival-scheduled_departure)/3600)::int, 
-		EXTRACT(minute from scheduled_arrival-scheduled_departure)) as duration
+		(EXTRACT(epoch from duration)/3600)::int, 
+		EXTRACT(minute from duration)) as duration
 FROM
-	flights
+	routes
 WHERE
-	 (scheduled_arrival-scheduled_departure) > interval '8 hours';
+	 duration >= interval '8 hours'
+ORDER BY duration;
 
+
+-- 4. Вывести всех клиентов без дубликатов, у которых есть номер телефона в БД. 
+--    Пример:  Valeriy Tikhonov, +70127117011
 SELECT
-	passenger_name,
+	initcap(passenger_name) passenger_name,
 	array_agg(contact_data->>'phone') phones_arr
 FROM
 	tickets
 WHERE
 	contact_data->>'phone' IS not NULL
-GROUP BY passenger_name;
+GROUP BY initcap(passenger_name);
 
+
+-- 5. Посчитать сколько всего пассажиров когда-то покупало билеты.
 SELECT
 	COUNT(DISTINCT passenger_name) num_passengers
 FROM
 	tickets;
 	
+
+-- 6.Посчитать количество мест для каждой модели самолета, 
+-- Пример: Аэробус А319-100, 20
 SELECT
 	a.model,
 	COALESCE(s.num_seats, 0) num_seats 
@@ -50,6 +65,11 @@ FROM Aircrafts a
 		FROM
 			seats
 		GROUP BY aircraft_code) s USING(aircraft_code);
+		
+		
+-- 7.7. Посчитать количество мест для каждой модели самолета, с разделением по классу, а 
+-- также отсортированному сначала по модели, а потом по классу. 
+-- Пример: Аэробус A319-100,Business,20
 SELECT
 	a.model,
 	COALESCE(s.fare_conditions, 'unknown') fare_conditions,
